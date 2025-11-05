f1269a9eb36ef36fc88c8b2b9708549f6c0e9fcb
Chengtai Wu <cw4483@nyu.edu>
2025-11-05 16:22:01 -0500

    set up api client

--- src/lib/yt-client.ts
+++ src/lib/yt-client.ts
@@ -1,5 +1,9 @@
 import { google } from 'googleapis';
 
-const oauth2Client = new google.auth.OAuth2(
+if (!process.env.YT_API_KEY) {
+  throw new Error("Missing Plaid credentials in environment variables");
+}
 
-);
+google.options({
+  auth: !process.env.YT_API_KEY ? undefined : process.env.YT_API_KEY,
+});
