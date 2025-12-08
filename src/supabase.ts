cb128e5bd014f5a7bdafba72bcbe1e6b1b7ac816
Chengtai Wu <cw4483@nyu.edu>
2025-12-08 15:20:20 -0500

    moved url to env

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
-const SUPERBASE_URL = 'https://capmwpaelrfdwmljlmbv.supabase.co';
+const SUPERBASE_URL = import.meta.env.VITE_SUPABASE_URL as string;
 
 export const supabaseClient = createClient(SUPERBASE_URL, SUPABASE_ANON_KEY);
