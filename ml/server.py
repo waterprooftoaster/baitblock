44c98e5e24b56b6c961c4d0800eba436e8bbab57
Chengtai Wu <cw4483@nyu.edu>
2025-12-08 19:46:21 -0500

    set up server for real time labeling

---
+++ ml/server.py
@@ -0,0 +1,19 @@
+from typing import List
+from fastapi import FastAPI
+from pydantic import BaseModel
+from label_messages import label 
+
+app = FastAPI()
+
+class ClassifyRequest(BaseModel):
+    texts: List[str]
+
+@app.post("/classify")
+def classify_endpoint(body: ClassifyRequest):
+    """
+    Accepts: {"texts": ["msg1", "msg2", ...]}
+    Returns: list of label dicts from label_messages.label()
+    """
+    results = label(body.texts)
+
+    return results
